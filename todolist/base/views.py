219b257a4ca78d4a909ecb3004ff055df1604bcd
Shivanswamynathan S <shiva7550240700@gmail.com>
2024-11-25 16:39:02 +0530

    LoginRequiredMixin

--- todolist/base/views.py
+++ todolist/base/views.py
@@ -5,6 +5,7 @@ from django.views.generic.edit import CreateView ,UpdateView,DeleteView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Task
 
@@ -18,26 +19,34 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
-class TaskDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
+       
+
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     #context_object_name = 'task'
     template_name = 'base/task.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model =Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
     model =Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
